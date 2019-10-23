70be264b6932f467dbf3e71c5165841df7f72296
ono2342 <56573437+ono2342@users.noreply.github.com>
2019-10-23 18:29:28 +0900

    グループ作成機能の実装とサイドバーへの表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,19 @@
 class GroupsController < ApplicationController
+def index
+end
+
 def new
-  @groups = Group.new
+  @group = Group.new
+  @group.users << current_user
 end
 
 def create
-  @groups =Group.new(group_params)
+  @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
 end
 
 def edit
@@ -12,5 +21,10 @@ end
 
 def update
 end
+
+private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
 
 end
