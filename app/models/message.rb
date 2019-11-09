46bb062c574ce4d0cf36609d198b11fdaec9ed88
ono2342 <56573437+ono2342@users.noreply.github.com>
2019-11-09 13:16:30 +0900

    commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :group
-  belongs_to :user
+  belongs_to :group, optional: true
+  belongs_to :user, optional: true
 
   validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
